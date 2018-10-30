07edb25714804072e38c24fa25efd1115dcc6318
Andreas Sandskär <ekrarna@gmail.com>
2018-10-30 09:48:31 +0100

    fungerar men jag tycker att platsen tar för lång tid

--- jquery_api/app.js
+++ jquery_api/app.js
@@ -4,7 +4,7 @@ var url
 //    Om vi hittar position så vill vi ha den såklart istället för standardvärdet ovan
 // samt förbereder förändring av avståndet
 var urlLat, urlLong
-var urlAvstand = 2000
+var urlAvstand = 1500
 
 $(function () {
     var options = {
@@ -22,6 +22,47 @@ $(function () {
           console.log(`Longitude: ${crd.longitude}`);
           urlLong = crd.longitude
         console.log(`More or less ${crd.accuracy} meters.`);
+
+        url = "https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=offentliga-toaletter&facet=plats&facet=sasong&facet=oppettider&facet=avgift&facet=antal_dam&facet=antal_herr&facet=antal_unisex&facet=antal_urinoar&facet=antal_hwc&facet=hwc_larm&facet=skotbord&geofilter.distance=" + urlLat + "%2C" + urlLong + "%2C" + urlAvstand;
+
+
+        // plan b. släng in koden här för request istället
+        $.getJSON(url, function (data) {
+
+            allt = data // ger mig tillgång till att felsöka datan som kommer in.
+        
+            for (let i = 0; i <= data.records.length - 1; i++) {
+                //      console.log(i)
+                //      console.log(data.records[i].fields)
+        
+                var items = ""
+                //          var items =[]
+        
+                $.each(data.records[i].fields, function (key, val) {
+        
+                    var platsen = data.records[i].fields.plats
+                    // platsen i filen ligger i allt[0].fields.plats
+                    $("<ul/>", { platsen }).appendTo("body")
+        
+                    //                items.push("<li id='" + key + "'>" + data.records[i].fields.plats + ": " + key + ": " + val + "</li>")
+                    items = ("<li id='" + key + "'>" + data.records[i].fields.plats + ": " + key + ": " + val + "</li>")
+        
+                    //objektlängden får göra om det så att det blir rätt senare
+                    //console.log(Object.keys(allt.records[i].fields).length)
+                 
+
+                    $("<ul/>", {
+                        "class": platsen,
+                        //                    html: items.join( "" )
+                        html: items
+        
+                    }).appendTo("body");
+                })
+            }
+        })
+        
+
+
       }
       
       function error(err) {
@@ -29,13 +70,6 @@ $(function () {
       }
 
       navigator.geolocation.getCurrentPosition(success, error, options);
-
-    if (urlLat != undefined){
-        url = "https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=offentliga-toaletter&facet=plats&facet=sasong&facet=oppettider&facet=avgift&facet=antal_dam&facet=antal_herr&facet=antal_unisex&facet=antal_urinoar&facet=antal_hwc&facet=hwc_larm&facet=skotbord&geofilter.distance=" + urlLat + "%2C" + urlLong + "%2C" + urlAvstand;
-    } else {
-    // Skapa en platsbaserad url. Detta är grundformen och kommer att vara utgångspunkten
-        url = "https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=offentliga-toaletter&facet=plats&facet=sasong&facet=oppettider&facet=avgift&facet=antal_dam&facet=antal_herr&facet=antal_unisex&facet=antal_urinoar&facet=antal_hwc&facet=hwc_larm&facet=skotbord&geofilter.distance=56.0467%2C12.6944%2C2000";
-    }
 
 /*    if (navigator.geolocation) {
     
@@ -53,36 +87,4 @@ $(function () {
     // $.getJSON("offentliga-toaletter.json", function(data) {
     // filen är helt annorlunda
 
-$.getJSON(url, function (data) {
-
-    allt = data // ger mig tillgång till att felsöka datan som kommer in.
-
-    for (let i = 0; i <= data.records.length - 1; i++) {
-        //      console.log(i)
-        //      console.log(data.records[i].fields)
-
-        var items = ""
-        //          var items =[]
-
-        $.each(data.records[i].fields, function (key, val) {
-
-            var platsen = data.records[i].fields.plats
-            // platsen i filen ligger i allt[0].fields.plats
-            $("<ul/>", { platsen }).appendTo("body")
-
-            //                items.push("<li id='" + key + "'>" + data.records[i].fields.plats + ": " + key + ": " + val + "</li>")
-            items = ("<li id='" + key + "'>" + data.records[i].fields.plats + ": " + key + ": " + val + "</li>")
-
-            //objektlängden får göra om det så att det blir rätt senare
-            console.log(Object.keys(allt.records[i].fields).length)
-
-            $("<ul/>", {
-                "class": platsen,
-                //                    html: items.join( "" )
-                html: items
-
-            }).appendTo("body");
-        })
-    }
-})
 })
