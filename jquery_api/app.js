005de3660e254d93cf260951bdce9431e26caa74
Andreas Sandskär <ekrarna@gmail.com>
2018-11-01 18:20:32 +0100

    nu fungerar sortering på timmar också!

--- jquery_api/app.js
+++ jquery_api/app.js
@@ -13,10 +13,10 @@ $(function () {
         console.log(plats.lon + " " + plats.lat)
         url = "https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=offentliga-toaletter&facet=plats&facet=sasong&facet=oppettider&facet=avgift&facet=antal_dam&facet=antal_herr&facet=antal_unisex&facet=antal_urinoar&facet=antal_hwc&facet=hwc_larm&facet=skotbord&geofilter.distance=" + plats.lat + "%2C" + plats.lon + "%2C" + urlAvstand;
         hamtaData(url)
-    }, hamtaPosGeo()) 
+    }, hamtaPosGeo())
 
     // ovan position vill inte fungera längre
-// hamtaPosGeo()
+    // hamtaPosGeo()
 
     $("#geoKnapp").click(function () {
         console.log("tryckt på uppdatera")
@@ -35,10 +35,10 @@ function hamtaPosGeo() {
     function success(pos) {
         var crd = pos.coords;
         /*
-                console.log('Din position:');
-                console.log(`Latitude : ${crd.latitude}`);
-                console.log(`Longitude: ${crd.longitude}`);
-                console.log(`More or less ${crd.accuracy} meters.`);
+        console.log('Din position:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
         */
         var urlLat = crd.latitude
         var urlLong = crd.longitude
@@ -59,101 +59,153 @@ function hamtaData(webadress) {
 
         allt = data // ger mig tillgång till att felsöka datan som kommer in.
 
+        // funktioner för att fixa tiduträkning för öppettiden
+        function delaStrang(strang, position) {
+            var a = strang
+            var position
+            return [a.slice(0, position), a.slice(position)]
+        }
+
+        // Gör en funktion för att lösa båda problemen med att visa Öppettider.
+        function addTecken(strang, position, tecken) {
+            var a = strang
+            var b = tecken
+            var position
+            return [a.slice(0, position), b, a.slice(position)].join('');
+        }
+
+        var dettaNu = new Date().getMonth()
+
+        var dettaKlockslag = new Date().getHours()
+        // oppnar, stanger refererar till månader
+        var oppnar, stanger
         for (let i = 0; i < data.records.length; i++) {
+
+            var kortare = data.records[i].fields
             // start here
-            // compare time of client with opengin hours and season
-            // opening hours = check time
-            // make client time visible
             // get the opeining season
             // split the string of the season
-            // calculate tha season range
-            // convert the clients month
-            var dettaNu = new Date().getMonth()
-            // compare clients month to season range
+            // possible strings
+            /*
+            helaaret
+            helaaretaprsep
+            helaaretmajsep
+            */
+            // det finns olika varianter här för vissa saker sker beroned på vad.
+            // tänker mig att det är ett span.
+            // nu är frågan vad menar de? är det öppet även i september?
+            // if (kortare.sasong == "aprsep"){ oppnar = 4; stanger = 9}
+
+            // oppet refererar till klockslag
+            oppet = kortare.oppettider
+
+
+            // compare time of client with open hours
+            // opening hours = check time
+            // make client time visible
+
+            // alternativ lösning
+            // allt.records[4].fields.sasong = "aprsep"
+            // dela till ["apr", "sep"]
+
             // if ( the date or time is out of range ignore) {
             // if there are no toilets widen the range of search
             // do search again with the new value
 
-            var items = []
-            var itemsLength = Object.keys(data.records[i].fields).length
 
-            var kortare = data.records[i].fields
 
-            // Vissa offentliga toaletter har en avgift
-            var avgiften = ""
-            if (kortare.avgift != undefined) {
-                avgiften = "<li> Avgift: " + kortare.avgift + "</li>"
+            // gjort en manuell variant för att ordna med alla datum
+            if (kortare.sasong.length == 6) {
+                if (kortare.sasong == "aprsep") { oppnar = 4; stanger = 9 }
+                if (kortare.sasong == "majokt") { oppnar = 5; stanger = 10 }
+                if (kortare.sasong == "majsep") { oppnar = 5; stanger = 9 }
+                if (kortare.sasong == "junsep") { oppnar = 6; stanger = 9 }
+
+                if (dettaNu < oppnar || dettaNu > stanger) {
+                    console.log(kortare.plats + " är stängd (" + kortare.sasong + ").")
+
+                }
+            } else if (kortare.oppettider.length == 4) {
+
+                // Tiden kan visas snyggare. Endast där det behövs
+                if (oppet.charAt(0) == 0) { // 0720 -> 07-20
+                    oppet = addTecken(oppet, 2, "-")
+                }
+
+
+                tiden = delaStrang(kortare.oppettider, 2)
+
+
+                var forstaTimman = kortare.oppettider.charAt(1)
+                if (dettaKlockslag > forstaTimman || dettaKlockslag < tiden[1]) {
+                    visaToa()
+                    //    console.log(dettaKlockslag +">"+ forstaTimman +"||"+ dettaKlockslag +"<"+ tiden[1])
+                    //       console.log(dettaKlockslag > forstaTimman || dettaKlockslag < tiden[1])
+
+                    //      console.log(kortare.plats + " är öppen (" + oppet[1] + ").")
+                } else {
+                    console.log(kortare.plats + " är stängd (" + kortare.oppettider + ").")
+
+                }
+            } else {
+                // console.log(kortare.plats + " är öppen (" + kortare.sasong + ").")
+                visaToa()
             }
-
-            // Tiden kan visas snyggare. Fixa skriva till hur långt kvar till att den stänger/öppnar.
-            oppet = kortare.oppettider
-            // Gör en funktion för att lösa båda problemen
-            function addTecken(strang, position, tecken) {
-                var a = strang
-                var b = tecken
-                var position
-                return [a.slice(0, position), b, a.slice(position)].join('');
-            }
-
-            // allt.records[4].fields.sasong = aprsep
-            // dela till [apr, sep]
-
-            var manader = delaDatum("aprsep", 3)
-
-            function delaDatum(strang, position){
-                var a = strang
-                var position
-                return [a.slice(0,position), a.slice(position)]
-
-                /*
-                var forstaOrd = []
-                var andraOrd
-                forstaOrd.push(strang.charAt(0)) // a
-                forstaOrd.push(strang.charAt(1)) // p
-                forstaOrd.push(strang.charAt(2)) // r
-
-                andraOrd[0] = strang.charAt(3) // s
-                andraOrd[1] = strang.charAt(4) // e
-                andraOrd[2] = strang.charAt(5) // e
-
-                return [forstaOrd,andraOrd] // [apr, se]
-            */
-            }
+            // nu behöver jag en funktion som gör detta nedan
+            function visaToa() {
 
 
-            if (oppet.charAt(0) == 0) { // 0720 -> 07-20
-                oppet = addTecken(oppet, 2, "-")
-            }
-            if (oppet.charAt(0) == "d") { // dygnetrunt -> dygnet runt
-                oppet = addTecken(oppet, 6, "\ ")
-            }
-            var sasongen = kortare.sasong
-            if (sasongen == "helaaret") { // helaaret -> Hela året
-                sasongen = "Hela året"
-            }
 
-            var platsen = kortare.plats
-            // här skrivs varje datainlägg
-            $("<div>", {
-                "class": "card-deck mb-3 text-left",
-                html:
-                    '<div class="card mb-4 shadow-sm"> \
+
+
+
+                // calculate tha season range
+                // convert the clients month
+                // compare clients month to season range
+
+
+
+                var itemsLength = Object.keys(data.records[i].fields).length
+
+
+                // Vissa offentliga toaletter har en avgift
+                var avgiften = ""
+                if (kortare.avgift != undefined) {
+                    avgiften = "<li> Avgift: " + kortare.avgift + "</li>"
+                }
+
+                // Tiden kan visas snyggare. Fixa skriva till hur långt kvar till att den stänger/öppnar.
+                if (oppet.charAt(0) == "d") { // dygnetrunt -> dygnet runt
+                    oppet = addTecken(oppet, 6, "\ ")
+                }
+                var sasongen = kortare.sasong
+                if (sasongen == "helaaret") { // helaaret -> Hela året
+                    sasongen = "Hela året"
+                }
+
+                var platsen = kortare.plats
+                // här skrivs varje datainlägg
+                $("<div>", {
+                    "class": "card-deck mb-3 text-left",
+                    html:
+                        '<div class="card mb-4 shadow-sm"> \
                 <div class="card-header"> \
                     <h4 class="my-0 font-weight-normal">'+ platsen + "</h4> \
                 </div> \
 <div class=\"card-body\">\
 <ul class=\"list-unstyled mt-3 mb-4\">"+
-                    "<li> Öppettider: " + oppet + "</li>" +
-                    "<li> Säsong: " + sasongen + "</li>" + avgiften +
-                    "</ul>" +
-                    "<a href=\'https://www.google.com/maps/search/?api=1&query=" + kortare.geo_point_2d + "'" +
-                    `   <button type="button" class="btn btn-lg btn-block btn-outline-primary"> \
+                        "<li> Öppettider: " + oppet + "</li>" +
+                        "<li> Säsong: " + sasongen + "</li>" + avgiften +
+                        "</ul>" +
+                        "<a href=\'https://www.google.com/maps/search/?api=1&query=" + kortare.geo_point_2d + "'" +
+                        `   <button type="button" class="btn btn-lg btn-block btn-outline-primary"> \
               Till google maps \
             </button>\
           </a>\
         </div>\
       </div>`
-            }).appendTo("article");
+                }).appendTo("article");
+            }
         }
     })
 }
